b3f176142de960a57136dac229e4dee4f69b9dda
Hongyi Zhao <hongyi.zhao@gmail.com>
2022-06-27 21:12:13 +0800

    new file:   bundling-extension.sh

---
+++ bundling-extension.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#https://github.com/feisele/vscode-gap-language-support/issues/5
+#$ proxychains-ng-http npm install
+npm install
+
+# Check the available scripts:
+#$ npm run
+#Scripts available in gap-language-support@0.1.1 via `npm run-script`:
+#  vscode:prepublish
+#    npm run compile
+#  compile
+#    tsc -p ./
+#  lint
+#    eslint . --ext .ts,.tsx
+#  watch
+#    tsc -watch -p ./
+
+# Then run the following:
+#$ proxychains-ng-http npm run vscode:prepublish
+npm run vscode:prepublish
+
+#$ ln -sfr vscode-gap-language-support.git/ ~/.vscode/extensions/
+ln -sfr vscode-gap-language-support.git/ ~/.vscode/extensions/
+
+#$ npm help install|grep -A4 -B2 -- -g
+#       • npm install (in a package directory, no arguments):
+#           Install the dependencies in the local node_modules folder.
+#           In global mode (ie, with -g or --global appended to the command),
+#           it installs the current package context (ie, the current working
+#           directory) as a global package.
+#           By default, npm install will install all modules listed as
+#           dependencies in npm help package.json.
